6cc7b088d08db608afde380c861a3205001a1ff2
Gerrit Gruben <gerrit.gruben@gmail.com>
2018-02-27 12:01:28 +0100

    Surpress annoying log msgs in simple_sparkstream

--- day1/simple_sparkstream/simple_sparkstream.py
+++ day1/simple_sparkstream/simple_sparkstream.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     # Create a local StreamingContext with two working thread
     # and batch interval of 1 second
     sc = SparkContext("local[1]", "Word Count")
+    sc.setLogLevel("FATAL")
     # 2nd argument is batch duration
     ssc = StreamingContext(sc, 10)
 
