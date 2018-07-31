bcb7a536087eaec58f4807f2e9ecb3728e4c3d4b
Gerrit Gruben <gerrit.gruben@gmail.com>
2018-07-31 12:56:49 +0200

    Simple Sparkstream
    - Version update of kafka package for simple spark streaming project (so
    it works with Spark 2.3.1)
    
    - added log surpression

--- day1/simple_sparkstream/simple_sparkstream.py
+++ day1/simple_sparkstream/simple_sparkstream.py
@@ -1,4 +1,4 @@
-#!/usr/bin env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from pyspark.streaming.kafka import KafkaUtils
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     # Create a local StreamingContext with two working thread
     # and batch interval of 1 second
     sc = SparkContext("local[1]", "Word Count")
+    sc.setLogLevel("WARN")
     # 2nd argument is batch duration
     ssc = StreamingContext(sc, 10)
 
